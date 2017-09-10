78f231521b213cdcbf81b375558441e791610d5e
Emil Kalimullin <ekalimullin@Emils-MacBook-Air.local>
2017-09-10 23:48:16 +0100

    Delete post with comment

--- app/models.py
+++ app/models.py
@@ -292,7 +292,8 @@ class Post(db.Model):
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    comments = db.relationship('Comment', backref='post', lazy='dynamic')
+    comments = db.relationship('Comment', backref='post', lazy='dynamic',
+                               cascade='all, delete-orphan')
 
     @staticmethod
     def generate_fake(count=100):
